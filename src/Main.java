8db47d48f42098b187b68284422046b1652f4fa6
cjayakrishna21@gmail.com <cjayakrishna21@gmail.com>
2023-02-11 02:16:57 +0530

    Second Update

--- src/Main.java
+++ src/Main.java
@@ -16,12 +16,18 @@ public class Main {
 
         JFrame frame = new JFrame("Simple frame");
         frame.setLayout(null);
-        frame.setBackground(Color.green);
+//        frame.setBackground(Color.green);
+
         frame.setSize(500,500);
         JButton jaya = new JButton("jaya");
         jaya.setBackground(Color.green);
         jaya.setBounds(100,0,100,20);
+        JButton krishna = new JButton("krishna");
+        krishna.setBackground(Color.yellow);
+        krishna.setBounds(220,100,100,20);
+
         frame.add(jaya);
+        frame.add(krishna);
         frame.setVisible(true);
 
     }
